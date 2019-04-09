862e409d8b9fb3d141d46a4847ff47c48615ac06
Erik S. Carlsten <ecarlste@gmail.com>
2019-04-09 00:21:08 -0600

    Configured server routes and redis implementation

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const { Pool } = require('pg');
+const redis = require('redis');
 
 const app = express();
 
@@ -22,3 +23,39 @@ pgClient.on('error', () => console.log('Lost PG connection...'));
 pgClient
   .query('CREATE TABLE IF NOT EXISTS values (number INT)')
   .catch(err => console.log(err));
+
+const redisClient = redis.createClient({
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000
+});
+const redisPublisher = redisClient.duplicate();
+
+app.get('/', (req, res) =>
+  res.send('An overly complex fibonacci sequence calculator!')
+);
+app.get('/values/all', async (req, res) => {
+  const values = await pgClient.query('SELECT * FROM values');
+
+  res.send(values.rows);
+});
+app.get('/values/current', async (req, res) => {
+  redisClient.hgetall('values', (err, values) => {
+    res.send(values);
+  });
+});
+app.get('/values', async (req, res) => {
+  const index = req.body.index;
+
+  if (parseInt(index) > 40) {
+    res.status(422).send('Index too high.');
+  }
+
+  redisClient.hset('values', index, 'Nothing yet!');
+  redisPublisher.publish('insert', index);
+  pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
+
+  res.send({ working: true });
+});
+
+app.listen(5000, () => console.log('Listening on port 5000...'));
